f086e6e10dba90c57797f48fa50bcb735b3da203
Heber Baraybar <heber.bar@gmail.com>
2022-01-30 13:16:14 +0100

    Fixing git

---
+++ cordinates.js
@@ -0,0 +1,12 @@
+let cordinates = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+const alive = 1;
+const dead = 0;
+for (let i = 0; i < cordinates.length; i++) {
+  for (let j = 0; j < cordinates[i].length; j++) {}
+}
---
+++ filtered-neighbor.js
@@ -0,0 +1,8 @@
+import { neighbors } from "./neighbor-parameters";
+
+export const filterNeighbors = myneighbors.filter(neighborsCondition){
+  
+}
+
+
+  
